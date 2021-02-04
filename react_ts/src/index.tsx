42ceee2d2e149f46f9bc2e0719c48f9fa0aa2197
wxb <superwang9527@163.com>
2021-02-04 14:31:22 +0800

    feat: 搭建完成

---
+++ react_ts/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './pages/app/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
---
+++ react_ts/src/pages/app/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+const App: FC<{}> = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
