3006280eb2c46913aa623381bfc4f6d6760f57be
wxb <superwang9527@163.com>
2021-02-19 23:17:58 +0800

    feat: 添加了sql文件

---
+++ koablog.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 80021
+Source Host           : localhost:3306
+Source Database       : koablog
+
+Target Server Type    : MYSQL
+Target Server Version : 80021
+File Encoding         : 65001
+
+Date: 2021-02-19 23:17:34
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
